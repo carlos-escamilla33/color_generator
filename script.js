ecf7633d81ba79e836d76677a07017dfb4058d66
Carlos Escamilla <71427987+carlos-escamilla33@users.noreply.github.com>
2021-08-01 19:11:01 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-var css = document.querySelector('h3');
-var color1 = document.querySelector('.color1');
-var color2 = document.querySelector('.color2');
-var body = document.getElementById('gradient');
+let css = document.querySelector('h3');
+let color1 = document.querySelector('.color1');
+let color2 = document.querySelector('.color2');
+let body = document.getElementById('gradient');
 
-function setGradient() {
+const setGradient = () => {
 	body.style.background = 'linear-gradient(to right, ' 
 	+ color1.value 
 	+ ', ' 
